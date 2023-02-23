79befb6f77b97965221284e4d35ae3895bbeb3e5
unknown <pablodiazsoley@gmail.com>
2023-02-23 18:08:37 +0100

    mas_ejemplos

--- ejemplos/deteccion_distancia.py
+++ ejemplos/deteccion_distancia.py
@@ -1,33 +1,43 @@
-import cv2                                # state of the art computer vision algorithms library
-import numpy as np                        # fundamental package for scientific computing
-import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
-import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
-print("Environment Ready")
+import pyrealsense2 as rs
 
-# Setup:
-pipe = rs.pipeline()
-cfg = rs.config()
-#cfg.enable_device_from_file("../object_detection.bag")
-profile = pipe.start(cfg)
+try:
+    # Create a context object. This object owns the handles to all connected realsense devices
+    pipeline = rs.pipeline()
 
-# Skip 5 first frames to give the Auto-Exposure time to adjust
-for x in range(5):
-  pipe.wait_for_frames()
-  
-# Store next frameset for later processing:
-frameset = pipe.wait_for_frames()
-color_frame = frameset.get_color_frame()
-depth_frame = frameset.get_depth_frame()
+    # Configure streams
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-# Cleanup:
-pipe.stop()
-print("Frames Captured")
+    # Start streaming
+    pipeline.start(config)
 
-color = np.asanyarray(color_frame.get_data())
-plt.rcParams["axes.grid"] = False
-plt.rcParams['figure.figsize'] = [12, 6]
-plt.imshow(color)
+    while True:
+        # This call waits until a new coherent set of frames is available on a device
+        # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+        frames = pipeline.wait_for_frames()
+        depth = frames.get_depth_frame()
+        if not depth: continue
 
-colorizer = rs.colorizer()
-colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
-plt.imshow(colorized_depth)
+        # Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
+        coverage = [0]*64
+        for y in range(480):
+            for x in range(640):
+                dist = depth.get_distance(x, y)
+                if 0 < dist and dist < 1:
+                    coverage[x//10] += 1
+            
+            if y%20 is 19:
+                line = ""
+                for c in coverage:
+                    line += " .:nhBXWW"[c//25]
+                coverage = [0]*64
+                print(line)
+    exit(0)
+#except rs.error as e:
+#    # Method calls agaisnt librealsense objects may throw exceptions of type pylibrs.error
+#    print("pylibrs.error was thrown when calling %s(%s):\n", % (e.get_failed_function(), e.get_failed_args()))
+#    print("    %s\n", e.what())
+#    exit(1)
+except Exception as e:
+    print(e)
+    pass
