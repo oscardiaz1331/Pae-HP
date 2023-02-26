d996fc67b8284d8fe1a037158ac2eaaf53a87f94
unknown <pablodiazsoley@gmail.com>
2023-02-26 17:20:29 +0100

    dos scripts nuevos. Uno para guardar un recording y el otro para enseñar ese recording

---
+++ calibrar_adquirir_datos/enseña_prerecorded_file.py
@@ -0,0 +1,37 @@
+import cv2                                # state of the art computer vision algorithms library
+import numpy as np                        # fundamental package for scientific computing
+import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
+import pyrealsense2 as rs  
+
+# Setup:
+pipe = rs.pipeline()
+cfg = rs.config()
+cfg.enable_device_from_file("../recording.bag")
+profile = pipe.start(cfg)
+
+# Skip 5 first frames to give the Auto-Exposure time to adjust
+for x in range(5):
+  pipe.wait_for_frames()
+  
+# Store next frameset for later processing:
+frameset = pipe.wait_for_frames()
+color_frame = frameset.get_color_frame()
+depth_frame = frameset.get_depth_frame()
+
+# Cleanup:
+pipe.stop()
+
+color = np.asanyarray(color_frame.get_data())
+colorizer = rs.colorizer()
+colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
+# Create alignment primitive with color as its target stream:
+align = rs.align(rs.stream.color)
+frameset = align.process(frameset)
+
+# Update color and depth frames:
+aligned_depth_frame = frameset.get_depth_frame()
+colorized_depth = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data())
+
+# Show the two frames together:
+images = np.hstack((color, colorized_depth))
+plt.imshow(images)
