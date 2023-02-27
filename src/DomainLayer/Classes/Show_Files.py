29014f39dbc1bd68b1f052e5528fabd221353113
alejandro.amat <alejandro.amat@d4115-1.upc.es>
2023-02-27 17:13:31 +0100

    show

--- src/DomainLayer/Classes/Show_Files.py
+++ src/DomainLayer/Classes/Show_Files.py
@@ -2,36 +2,52 @@ import cv2                                # state of the art computer vision alg
 import numpy as np                        # fundamental package for scientific computing
 import pyrealsense2 as rs  
 
-# Setup:
-pipe = rs.pipeline()
-cfg = rs.config()
-rs.config.enable_device_from_file(cfg , "./recroding.bag")
-cfg.enable_stream(rs.stream.depth, rs.format.z16, 30)
-profile = pipe.start(cfg)
 
-cv2.namedWindow("Depth Stream", cv2.WINDOW_AUTOSIZE)
+
+
+class Show_Files:
+   
+    def __init__ (self):
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+
+
+
+    def enable_file (self, file):
+         
+
+        self.config.enable_device_from_file(self.config , file)
+        self.enable_stream(rs.stream.depth, rs.format.z16, 30)
+        profile = self.pipe.start(self.config)
+
+
+
+    def show (self):
+
+        cv2.namedWindow("Depth Stream", cv2.WINDOW_AUTOSIZE)
   
-# Create colorizer object
-colorizer = rs.colorizer()
+        # Create colorizer object
+        colorizer = rs.colorizer()
 
-    # Streaming loop
-while True:
-        # Get frameset of depth
-        frames = pipe.wait_for_frames()
+            # Streaming loop
+        while True:
+                # Get frameset of depth
+                frames = self.pipe.wait_for_frames()
 
-        # Get depth frame
-        depth_frame = frames.get_depth_frame()
+                # Get depth frame
+                depth_frame = frames.get_depth_frame()
 
-        # Colorize depth frame to jet colormap
-        depth_color_frame = colorizer.colorize(depth_frame)
+                # Colorize depth frame to jet colormap
+                depth_color_frame = colorizer.colorize(depth_frame)
 
-        # Convert depth_frame to numpy array to render image in opencv
-        depth_color_image = np.asanyarray(depth_color_frame.get_data())
+                # Convert depth_frame to numpy array to render image in opencv
+                depth_color_image = np.asanyarray(depth_color_frame.get_data())
 
-        # Render image in opencv window
-        cv2.imshow("Depth Stream", depth_color_image)
-        key = cv2.waitKey(1)
-        # if pressed escape exit program
-        if key == 27:
-            cv2.destroyAllWindows()
-            break
+                # Render image in opencv window
+                cv2.imshow("Depth Stream", depth_color_image)
+                key = cv2.waitKey(1)
+                # if pressed escape exit program
+                if key == 27:
+                    cv2.destroyAllWindows()
+                    break
