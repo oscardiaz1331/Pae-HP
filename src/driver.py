081958b947831b91fb089b6f432b6bb766cbaecf
alejandro.amat <alejandro.amat@d4115-1.upc.es>
2023-03-02 17:15:05 +0100

    s

--- src/driver.py
+++ src/driver.py
@@ -1,7 +1,9 @@
 import Construct_verification as cr
+filesource = "./src/DataLayer/"
+
 
 print("Realsense object validator driver")
 
 verif = cr.Construct_verification()
 
-verif.show_files("./src/DataLayer/prueba.bag")
+verif.show_files(filesource+ "prueba.bag")
