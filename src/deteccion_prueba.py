ead1ade65a94703fe074dfcba2bc5a6b8e37d8da
unknown <pablodiazsoley@gmail.com>
2023-03-06 17:17:10 +0100

    base de datos

--- src/deteccion_prueba.py
+++ src/deteccion_prueba.py
@@ -2,7 +2,7 @@ import Construct_verification as cr
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt 
-
+import tensorflow as tf
 
 print("Realsense object validator driver")
 
@@ -12,41 +12,3 @@ depth_frame, color_frame=cv.show_files(20,"./src/DataLayer/recording2.bag")
 color_data = np.asanyarray(color_frame.get_data())
 depth_data = np.asanyarray(depth_frame.get_data())
 
-
-
-height, width = color_data.shape[:2]
-expected = 300
-aspect = width / height
-resized_image = cv2.resize(color_data, (round(expected * aspect), expected))
-crop_start = round(expected * (aspect - 1) / 2)
-crop_img = resized_image[0:expected, crop_start:crop_start+expected]
-
-net = cv2.dnn.readNetFromCaffe("./src/DataLayer/MobileNetSSD_deploy.prototxt", "./src/DataLayer/MobileNetSSD_deploy.caffemodel")
-inScaleFactor = 0.007843
-meanVal       = 127.53
-classNames = ("background", "aeroplane", "bicycle", "bird", "boat",
-              "bottle", "bus", "car", "cat", "chair",
-              "cow", "diningtable", "dog", "horse",
-              "motorbike", "person", "pottedplant",
-              "sheep", "sofa", "train", "tvmonitor")
-
-
-blob = cv2.dnn.blobFromImage(crop_img, inScaleFactor, (expected, expected), meanVal, False)
-net.setInput(blob, "data")
-detections = net.forward("detection_out")
-
-label = detections[0,0,0,1]
-conf  = detections[0,0,0,2]
-xmin  = detections[0,0,0,3]
-ymin  = detections[0,0,0,4]
-xmax  = detections[0,0,0,5]
-ymax  = detections[0,0,0,6]
-className = classNames[int(label)]
-
-cv2.rectangle(crop_img, (int(xmin * expected), int(ymin * expected)), 
-             (int(xmax * expected), int(ymax * expected)), (255, 255, 255), 2)
-cv2.putText(crop_img, className, 
-            (int(xmin * expected), int(ymin * expected) - 5),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,255,255))
-
-plt.imshow(crop_img)
