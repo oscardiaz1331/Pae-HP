22abb66c338f4d5cee91110436ce2f9b5a119bf1
Felipm6 <felipm06@gmail.com>
2023-03-13 17:59:17 +0100

    prueba

---
+++ hpverif/label_distance.py
@@ -0,0 +1,20 @@
+
+
+class Label_Distance():
+
+    def __int__(self):
+        pass
+
+    def assign_label(self, matrizDist, labels, k):
+        k=k+1
+        vect=[]
+        wid = len(matrizDist)
+        heigh = len(matrizDist[0])
+        
+        vect=[0 for i in range(k)]
+        for height in range (0, heigh):
+                    for width in range (0, wid):
+                          #if label tal
+                           # vect[] +=
+                        
+
