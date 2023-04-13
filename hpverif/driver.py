3130ddfaf2a07f7ba5f05c58d3fefe92d660974c
unknown <pablodiazsoley@gmail.com>
2023-04-13 19:53:04 +0200

    cositas

--- hpverif/driver.py
+++ hpverif/driver.py
@@ -5,5 +5,5 @@ import verification  as v
 verif = v.Verification()
 dp,dc = verif.capture_frame(100, "gg.bag", 0)
 dp,dc,p = verif.show_files(26,"gg.bag")
-verif.PointCloud(dp,"gg")
-verif.Segmentation("./gg/gg.ply")
+verif.PointCloud(dp,"gg_pared")
+#verif.Segmentation("./gg/gg.ply")
--- hpverif/pointCloud.py
+++ hpverif/pointCloud.py
@@ -44,7 +44,8 @@ class PointCloud:
 
         print(depth_o3d)
         # Create a depth image from the numpy array
-        intrinsic = o3d.camera.PinholeCameraIntrinsic(640, 480, 525, 525, 320, 240)
+
+        intrinsic = o3d.camera.PinholeCameraIntrinsic(640, 480, 616.962, 616.962, 329.215, 237.106)
         im = o3d.geometry.PointCloud.create_from_depth_image(depth_o3d,intrinsic)
 
         o3d.visualization.draw_geometries([im])
