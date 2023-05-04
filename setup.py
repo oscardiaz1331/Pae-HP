b8907ded25af2b489853c1ad72e4362c6278e5b5
alejandro.amat <alejandro.amat@d4115-1.upc.es>
2023-05-04 19:15:55 +0200

    a

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from setuptools import find_packages, setup
 setup(
     name='hpverif',
     packages=find_packages(include=['hpverif']),
-    version='1.6.3',
+    version='2.5.3',
     install_requires = [
     'pyrealsense2',
     'numpy',
     'opencv-python',
     'matplotlib',
     'scipy',
-    'sklearn',
     'open3d',
-    'imageio',
-    'ast'
+    'imageio'
     ],
     description='Construction verification library PAE 2023',
     author='Telecos PAE 2023 Students',
